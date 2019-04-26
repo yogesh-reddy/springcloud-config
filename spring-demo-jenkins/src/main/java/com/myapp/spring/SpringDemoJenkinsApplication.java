5da4c3815b7e931cd4e3aae008fb495f3823246c
Krishnamurthy Pradeep <pradeeepmurthy@gmail.com>
2019-04-26 14:40:50 +0530

    jenkins-springboot project added

---
+++ spring-demo-jenkins/src/main/java/com/myapp/spring/SpringDemoJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoJenkinsApplication.class, args);
+	}
+
+}
